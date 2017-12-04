2266a731d3a1dea9bc445d2196bdc69a431be64a
Guntur Poetra <poetra.guntur@gmail.com>
2017-12-04 11:41:37 +0700

    enforce esnext syntax and conventions

--- index.js
+++ index.js
@@ -3,32 +3,22 @@ const path = require('path');
 const globby = require('globby');
 const isSymbolicLink = require('is-symbolic-link');
 
-module.exports = function (patterns, opts) {
+module.exports = (patterns, opts) => {
 	opts = Object.assign({}, opts);
 
 	return globby(patterns, opts)
-		.then(files => {
-			return Promise.all(files.map(fp => {
-				fp = path.resolve(opts.cwd || '', fp);
+		.then(files => Promise.all(files.map(fp => {
+			fp = path.resolve(opts.cwd || '', fp);
 
-				return isSymbolicLink(fp).then(val => {
-					return val ? fp : null;
-				});
-			}));
-		})
-		.then(fp => {
-			return fp.filter(Boolean);
-		});
+			return isSymbolicLink(fp).then(val => val ? fp : null);
+		})))
+		.then(fp => fp.filter(Boolean));
 };
 
-module.exports.sync = function (patterns, opts) {
+module.exports.sync = (patterns, opts) => {
 	opts = Object.assign({}, opts);
 
 	return globby.sync(patterns, opts)
-		.map(fp => {
-			return path.resolve(opts.cwd || '', fp);
-		})
-		.filter(fp => {
-			return isSymbolicLink.sync(fp);
-		});
+		.map(fp => path.resolve(opts.cwd || '', fp))
+		.filter(fp => isSymbolicLink.sync(fp));
 };
